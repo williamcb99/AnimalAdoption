f4a80821d0ff59fb273c87f2855f44beb697a0e1
william.calissendorff.brodd@gmail.com <william.calissendorff.brodd@gmail.com>
2025-09-14 12:54:26 +0200

    Began specifying animal

---
+++ AnimalAdoption.Api/Enums/AnimalType.cs
@@ -0,0 +1,5 @@
+public enum AnimalType
+{
+    Cat = 1,
+    Dog
+}
---
+++ AnimalAdoption.Api/Models/Animal.cs
@@ -0,0 +1,10 @@
+public class Animal
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public AnimalType Type { get; set; }
+    public int Age { get; set; }
+    public string Breed { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public bool IsAdopted { get; set; }
+}
