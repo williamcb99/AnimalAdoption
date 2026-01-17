7e65accc7332b31c716b291caa9ae9fe92ae1480
william.calissendorff.brodd@gmail.com <william.calissendorff.brodd@gmail.com>
2026-01-17 18:05:46 +0100

    Minor fix to use a constant for the dummy password hash

--- AnimalAdoption.Api/Controllers/AuthenticationController.cs
+++ AnimalAdoption.Api/Controllers/AuthenticationController.cs
@@ -6,6 +6,7 @@ public class AuthenticationController : ControllerBase
 {
     private readonly JwtTokenService _jwtTokenService;
     private readonly IUserRepository _userRepository;
+    private const string DummyHash = "$2a$12$d1F4sPK4vm8gOjE./Ji8suSH.ytMQlJxHn.jWVTOf.hZPEA4NQogG";
     public AuthenticationController(JwtTokenService jwtTokenService, IUserRepository userRepository)
     {
         _jwtTokenService = jwtTokenService;
@@ -24,7 +25,7 @@ public class AuthenticationController : ControllerBase
 
         var user = await _userRepository.GetUserByUsernameAsync(auth.Username);
 
-        var hash = user?.PasswordHash ?? "$2a$12$d1F4sPK4vm8gOjE./Ji8suSH.ytMQlJxHn.jWVTOf.hZPEA4NQogG";
+        var hash = user?.PasswordHash ?? DummyHash;
 
         if (!BCrypt.Net.BCrypt.Verify(auth.Password, hash))
             return Unauthorized(new ProblemDetails
